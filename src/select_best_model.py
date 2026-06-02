0960009501d3749c747b30fb9a8824f4ef4a37ac
Azza <azzabennejma0@gmail.com>
2026-06-02 03:25:40 +0100

    feat: add ngrok MLflow tracking for GitHub Actions

--- src/select_best_model.py
+++ src/select_best_model.py
@@ -73,7 +73,7 @@ else:
 # ==================================================
 import mlflow
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "http://127.0.0.1:5000"))
 mlflow.set_experiment("car-damage-detection")
 
 with mlflow.start_run():
--- src/train.py
+++ src/train.py
@@ -66,6 +66,8 @@ else:
 # ==================================================
 # START MLFLOW RUN
 # ==================================================
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "http://127.0.0.1:5000"))
+mlflow.set_experiment("car-damage-detection")
 
 with mlflow.start_run():
 
