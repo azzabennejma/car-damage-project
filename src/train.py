d1e78703524f1a2bc18600585f5e08678dc4f361
Azza <azzabennejma0@gmail.com>
2026-06-18 14:23:29 +0100

    Update training pipeline and MLflow integration

--- src/train.py
+++ src/train.py
@@ -59,7 +59,7 @@ else:
 # ==================================================
 # START MLFLOW RUN
 # ==================================================
-mlflow.set_tracking_uri("http://127.0.0.1:4040")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("car-damage-detection")
 
 with mlflow.start_run():
