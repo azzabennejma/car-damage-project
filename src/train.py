bc5e8e90053a41a2f52e7336847285fdfceb4894
Azza <azzabennejma0@gmail.com>
2026-06-18 01:45:23 +0100

    Fix MLflow tracking URI issue

--- src/train.py
+++ src/train.py
@@ -59,7 +59,7 @@ else:
 # ==================================================
 # START MLFLOW RUN
 # ==================================================
-mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_tracking_uri("http://127.0.0.1:4040")
 mlflow.set_experiment("car-damage-detection")
 
 with mlflow.start_run():
